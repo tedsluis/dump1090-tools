07ad7467f478760a9226ad538ed259b755cccb5e
tedsluis <ted.sluis@gmail.com>
2016-01-17 23:42:46 +0100

    removed old files

--- mrtg/fetch-dump1090-message-count.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-COUNTER=`wget -q -O - "$1/data/aircraft.json" | jq '.messages'`
-
-if [ -n "$COUNTER" ]; then echo $COUNTER; else echo UNKNOWN; fi
-echo 0
-echo 0
-echo "dump1090 at $1"
