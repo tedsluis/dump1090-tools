b2deeced1f7f4225cf47989c0ffc9cf34b2025e2
osmanovv <osmanovv@gmail.com>
2017-03-20 10:31:59 +0300

    forget to add width & height params to render full graph images

--- make-graphs.sh
+++ make-graphs.sh
@@ -385,15 +385,15 @@ diskio_ops_graph() {
 }
 
 common_graphs() {
-  aircraft_graph     /var/www/collectd/dump1090-$2-acs-$4.png    /var/lib/collectd/rrd/$1/dump1090-$2            "$3" "$4" "$5"
-  cpu_graph          /var/www/collectd/dump1090-$2-cpu-$4.png    /var/lib/collectd/rrd/$1/dump1090-$2            "$3" "$4" "$5"
-  tracks_graph       /var/www/collectd/dump1090-$2-tracks-$4.png /var/lib/collectd/rrd/$1/dump1090-$2            "$3" "$4" "$5"
-  metric_range_graph /var/www/collectd/dump1090-$2-range-$4.png  /var/lib/collectd/rrd/$1/dump1090-$2            "$3" "$4" "$5"
-  signal_graph       /var/www/collectd/dump1090-$2-signal-$4.png /var/lib/collectd/rrd/$1/dump1090-$2            "$3" "$4" "$5"
-  local_rate_graph   /var/www/collectd/dump1090-$2-rate-$4.png   /var/lib/collectd/rrd/$1/dump1090-$2            "$3" "$4" "$5"
-  net_graph          /var/www/collectd/dump1090-$2-eth0-$4.png   /var/lib/collectd/rrd/localhost/interface-eth0  "$3" "$4" "$5"
-  memory_graph       /var/www/collectd/dump1090-$2-memory-$4.png /var/lib/collectd/rrd/$1/memory                 "$3" "$4" "$5"
-  diskio_ops_graph   /var/www/collectd/dump1090-$2-disk-$4.png   /var/lib/collectd/rrd/$1/disk-mmcblk0           "$3" "$4" "$5"
+  aircraft_graph     /var/www/collectd/dump1090-$2-acs-$4.png    /var/lib/collectd/rrd/$1/dump1090-$2            "$3" "$4" "$5" "$6" "$7"
+  cpu_graph          /var/www/collectd/dump1090-$2-cpu-$4.png    /var/lib/collectd/rrd/$1/dump1090-$2            "$3" "$4" "$5" "$6" "$7"
+  tracks_graph       /var/www/collectd/dump1090-$2-tracks-$4.png /var/lib/collectd/rrd/$1/dump1090-$2            "$3" "$4" "$5" "$6" "$7"
+  metric_range_graph /var/www/collectd/dump1090-$2-range-$4.png  /var/lib/collectd/rrd/$1/dump1090-$2            "$3" "$4" "$5" "$6" "$7"
+  signal_graph       /var/www/collectd/dump1090-$2-signal-$4.png /var/lib/collectd/rrd/$1/dump1090-$2            "$3" "$4" "$5" "$6" "$7"
+  local_rate_graph   /var/www/collectd/dump1090-$2-rate-$4.png   /var/lib/collectd/rrd/$1/dump1090-$2            "$3" "$4" "$5" "$6" "$7"
+  net_graph          /var/www/collectd/dump1090-$2-eth0-$4.png   /var/lib/collectd/rrd/localhost/interface-eth0  "$3" "$4" "$5" "$6" "$7"
+  memory_graph       /var/www/collectd/dump1090-$2-memory-$4.png /var/lib/collectd/rrd/$1/memory                 "$3" "$4" "$5" "$6" "$7"
+  diskio_ops_graph   /var/www/collectd/dump1090-$2-disk-$4.png   /var/lib/collectd/rrd/$1/disk-mmcblk0           "$3" "$4" "$5" "$6" "$7"
 }
 
 common_graphs_preview() {
@@ -403,7 +403,7 @@ common_graphs_preview() {
   metric_range_graph /var/www/collectd/dump1090-$2-range-preview-$4.png  /var/lib/collectd/rrd/$1/dump1090-$2            "$3" "$4" "$5" "$6" "$7"
   signal_graph       /var/www/collectd/dump1090-$2-signal-preview-$4.png /var/lib/collectd/rrd/$1/dump1090-$2            "$3" "$4" "$5" "$6" "$7"
   local_rate_graph   /var/www/collectd/dump1090-$2-rate-preview-$4.png   /var/lib/collectd/rrd/$1/dump1090-$2            "$3" "$4" "$5" "$6" "$7"
-  net_graph          /var/www/collectd/dump1090-$2-eth0-preview-$4.png  /var/lib/collectd/rrd/localhost/interface-eth0  "$3" "$4" "$5" "$6" "$7"
+  net_graph          /var/www/collectd/dump1090-$2-eth0-preview-$4.png  /var/lib/collectd/rrd/localhost/interface-eth0   "$3" "$4" "$5" "$6" "$7"
   memory_graph       /var/www/collectd/dump1090-$2-memory-preview-$4.png /var/lib/collectd/rrd/$1/memory                 "$3" "$4" "$5" "$6" "$7"
   diskio_ops_graph   /var/www/collectd/dump1090-$2-disk-preview-$4.png   /var/lib/collectd/rrd/$1/disk-mmcblk0           "$3" "$4" "$5" "$6" "$7"
 }
