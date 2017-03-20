6ee907a7d4e778b37d8eee00be1aab440af535ef
osmanovv <osmanovv@gmail.com>
2017-03-20 09:52:09 +0300

    wlan0 => eth0 for preview

--- make-graphs.sh
+++ make-graphs.sh
@@ -403,7 +403,7 @@ common_graphs_preview() {
   metric_range_graph /var/www/collectd/dump1090-$2-range-preview-$4.png  /var/lib/collectd/rrd/$1/dump1090-$2            "$3" "$4" "$5" "$6" "$7"
   signal_graph       /var/www/collectd/dump1090-$2-signal-preview-$4.png /var/lib/collectd/rrd/$1/dump1090-$2            "$3" "$4" "$5" "$6" "$7"
   local_rate_graph   /var/www/collectd/dump1090-$2-rate-preview-$4.png   /var/lib/collectd/rrd/$1/dump1090-$2            "$3" "$4" "$5" "$6" "$7"
-  net_graph          /var/www/collectd/dump1090-$2-wlan0-preview-$4.png  /var/lib/collectd/rrd/localhost/interface-wlan0  "$3" "$4" "$5" "$6" "$7"
+  net_graph          /var/www/collectd/dump1090-$2-eth0-preview-$4.png  /var/lib/collectd/rrd/localhost/interface-eth0  "$3" "$4" "$5" "$6" "$7"
   memory_graph       /var/www/collectd/dump1090-$2-memory-preview-$4.png /var/lib/collectd/rrd/$1/memory                 "$3" "$4" "$5" "$6" "$7"
   diskio_ops_graph   /var/www/collectd/dump1090-$2-disk-preview-$4.png   /var/lib/collectd/rrd/$1/disk-mmcblk0           "$3" "$4" "$5" "$6" "$7"
 }
